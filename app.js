5525820fa53fd69da4b9d0ea8e3c549242c00162
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-27 17:48:27 +1000

    add ordinal scale and some data

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let timeScale = d3
   .scaleTime()
   .domain([new Date(2022, 0, 1), new Date()])
   .range([0, 100]);
-console.log("Time Scale");
+console.log("### Time Scale");
 console.log(timeScale(new Date(2022, 5, 27)));
 
 // Quantize Scale
@@ -19,7 +19,15 @@ let quantizeScale = d3
   .scaleQuantize()
   .domain([0, 100])
   .range(["yellow", "green"]);
-console.log("Quantize Scale");
+console.log("### Quantize Scale");
 console.log(quantizeScale(90));
 console.log(quantizeScale(49));
 console.log(quantizeScale(12));
+
+// Ordinal Scale
+let ordinalScale = d3
+  .scaleOrdinal()
+  .domain(["good", "great", "excellent"])
+  .range(["red", "black", "beige"]);
+console.log("### Ordinal Scale");
+console.log(ordinalScale("good"));
