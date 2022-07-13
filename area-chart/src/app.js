ad74eb159dc90522a849d725ac1170f8da09deed
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-13 17:41:32 +1000

    finish area chart

--- area-chart/src/app.js
+++ area-chart/src/app.js
@@ -9,22 +9,23 @@ var svg = d3
   .attr("height", height + margin.top + margin.bottom)
   .call(responsivefy)
   .append("g")
-  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.json("./data.json", function (err, data) {
-  let parseTime = d3.timeParse("%Y/%m/%d");
+  var parseTime = d3.timeParse("%Y/%m/%d");
+
   data.forEach((company) => {
     company.values.forEach((d) => {
       d.date = parseTime(d.date);
-      d.clost = +d.close;
+      d.close = +d.close;
     });
   });
 
-  let xScale = d3
+  var xScale = d3
     .scaleTime()
     .domain([
       d3.min(data, (co) => d3.min(co.values, (d) => d.date)),
-      d3.min(data, (co) => d3.max(co.values, (d) => d.date)),
+      d3.max(data, (co) => d3.max(co.values, (d) => d.date)),
     ])
     .range([0, width]);
   svg
@@ -32,14 +33,33 @@ d3.json("./data.json", function (err, data) {
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(xScale).ticks(5));
 
-  let yScale = d3
+  var yScale = d3
     .scaleLinear()
     .domain([
       d3.min(data, (co) => d3.min(co.values, (d) => d.close)),
-      d3.min(data, (co) => d3.max(co.values, (d) => d.close)),
+      d3.max(data, (co) => d3.max(co.values, (d) => d.close)),
     ])
     .range([height, 0]);
   svg.append("g").call(d3.axisLeft(yScale));
+
+  var area = d3
+    .area()
+    .x((d) => xScale(d.date))
+    .y0(yScale(yScale.domain()[0]))
+    .y1((d) => yScale(d.close))
+    .curve(d3.curveCatmullRom.alpha(0.5));
+
+  svg
+    .selectAll(".area")
+    .data(data)
+    .enter()
+    .append("path")
+    .attr("class", "area")
+    .attr("d", (d) => area(d.values))
+    .style("stroke", (d, i) => ["#FF9900", "#3369E8"][i])
+    .style("stroke-width", 2)
+    .style("fill", (d, i) => ["#FF9900", "#3369E8"][i])
+    .style("fill-opacity", 0.5);
 });
 
 function responsivefy(svg) {
