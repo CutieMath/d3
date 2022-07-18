2a0768c519b036cd1c1a0623c70d22e627d567d1
Yuxin Ye <yuxin_ye@protonmail.com>
2022-07-18 17:25:08 +1000

    put all the transition elements into one function

--- animation/src/app.js
+++ animation/src/app.js
@@ -1,3 +1,9 @@
-let t = d3.transition().delay(1000).duration(1000);
+// set transition rules
+var t = d3.transition().delay(1000).duration(1000);
 
-d3.selectAll(".block").transition(t).style("width", "400px");
+function go() {
+  // change selected elements
+  d3.selectAll(".block").transition(t).style("width", "400px");
+  d3.select(".a").transition(t).style("background", "pink");
+  d3.select(".b").transition(t).style("background", "gray");
+}
